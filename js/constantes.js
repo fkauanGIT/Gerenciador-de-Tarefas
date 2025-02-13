2b6e1fcc9c419baab6e3cc4f4bf7609b4a6c6d68
fkauanGIT <kauanuniversity@gmail.com>
2025-02-12 21:39:03 -0300

    Melhorei a legibilidade das classes HTMl para melhor entendimento !

--- js/constantes.js
+++ js/constantes.js
@@ -6,7 +6,7 @@ export const setPriorityColor = (taskElement) => {
     const corLaranja = "#FFBD3D";
     const corVerde = "#72C240";
 
-    const priorityElement = taskElement.querySelector(".priority");
+    const priorityElement = taskElement.querySelector(".task-item__priority");
 
     if (priorityElement) {
         const priority = priorityElement.textContent.trim();  // Obtém a prioridade do texto
@@ -23,7 +23,7 @@ export const setPriorityColor = (taskElement) => {
 
 //Função para organizar por prioridade, data e criar o li
 export const arrangeAccordingToOrder = () => {
-    const taskList = document.querySelector(".contents");
+    const taskList = document.querySelector(".tasks-list");
 
 
     taskLi.sort((a, b) => {
@@ -45,41 +45,39 @@ export const arrangeAccordingToOrder = () => {
     taskList.innerHTML = ''
     taskLi.forEach(task => {
         const Task = document.createElement("li");
-        Task.classList.add("containerTask");
+        Task.classList.add("task");
         Task.setAttribute("data-id", task.id);
 
         Task.innerHTML = `
-        <div class="div_containerTask">
-            <div class="informationTask">
-                <div class="priority priority-${task.priority.toLowerCase()}">${task.priority}</div>
-                <div class="date">${task.date}</div>
-                <ul class="responsabilitys">
-                    ${task.responsabilities.map(responsible => `<li class="name">${responsible}</li>`).join('')}
-                </ul>
+        <div class="task-item">
+            <div class="task-item__information">
+                <div class="task-item__priority task-item__priority--${task.priority.toLowerCase()}">${task.priority}</div>
+                <div class="task-item__date">${task.date}</div>
+                <ul class="task-item__responsibles">${task.responsabilities.map((responsible) =>`<li class="task-item__responsible-name">${responsible}</li>`).join("")}</ul>
             </div>
-            <div class="nameDescriptionTask">
-                <h2 class="title">${task.title}</h2>
-                <p class="description">${task.description}</p>
+            <div class="task-item__description">
+                <h2 class="task-item__title">${task.title}</h2>
+                <p class="task-item__text-description">${task.description}</p>
             </div>
         </div>
 
-        <div class="trash">
-                <svg class="delete-task bi bi-trash3" data-id="${task.id}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                </svg>
+        <div class="task-item__actions">
+            <svg class="task-item__delete-icon bi bi-trash3" data-id="${task.id}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+            </svg>
 
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="task-item__check-icon bi bi-check-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </svg>
         </div>
         `;
 
         taskList.appendChild(Task);
     })
 
-    document.querySelectorAll(".delete-task").forEach(trash => {
+    document.querySelectorAll(".task-item__delete-icon").forEach(trash => {
         trash.addEventListener("click", (event) => {
-            const taskElement = event.target.closest(".containerTask"); 
+            const taskElement = event.target.closest(".task"); 
             const taskId = taskElement.getAttribute("data-id");
     
             if (taskElement) {
@@ -90,18 +88,18 @@ export const arrangeAccordingToOrder = () => {
         });
     });
 
-    document.querySelectorAll(".containerTask").forEach(setPriorityColor)
+    document.querySelectorAll(".task").forEach(setPriorityColor)
 }
 
 //Função de criar fomulário
 export const createFormAdd = () => {
-    const title_form = document.getElementById("title_form").value;
+    const title_form = document.getElementById("task-title").value;
     const priority = document.getElementById("priority").value;
-    const date = document.getElementById("date").value;
-    const description = document.getElementById("description").value;
+    const date = document.getElementById("task-date").value;
+    const description = document.getElementById("task-description").value;
 
     const responsabilitys = [];
-    const checkboxes = document.querySelectorAll("input[name='responsability']:checked");
+    const checkboxes = document.querySelectorAll("input[name='responsibility']:checked");
     checkboxes.forEach(checkbox => {
         responsabilitys.push(checkbox.value);
     });
